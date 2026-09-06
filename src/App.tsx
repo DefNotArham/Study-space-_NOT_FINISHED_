3ec2fadca4d11635da174cae5953fb0f1d705d34
DefNotArham <arhamkabir231@gmail.com>
2026-09-05 20:00:28 -0400

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
