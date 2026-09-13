b483a53620849f478cbd135a3bbfdfdb778a69cb
DefNotArham <arhamkabir231@gmail.com>
2026-09-13 17:48:22 -0400

    basic create tasks func

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,5 +1,33 @@
+import { useAuthStore } from "../stores/auth.store";
+import { supabase } from "../lib/supabase";
+
 const TasksPage = () => {
-  return <div></div>;
+  const user = useAuthStore((state) => state.user);
+
+  const createTask = async () => {
+    const { data, error } = await supabase.from("tasks").insert({
+      user_id: user?.id,
+      title: "My first task",
+      priority: "high",
+    });
+
+    console.log(data);
+    console.log(error);
+  };
+
+  return (
+    <div className="p-10">
+      <h1>Task</h1>
+      <div></div>
+
+      <button
+        className="cursor-pointer bg-red-500 text-white px-3 py-2 rounded-2xl mt-10"
+        onClick={createTask}
+      >
+        Add tasks
+      </button>
+    </div>
+  );
 };
 
 export default TasksPage;
