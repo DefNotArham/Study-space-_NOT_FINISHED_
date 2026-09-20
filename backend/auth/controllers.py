03e97de89e031307a2824ac37c896944e53a3a1c
DefNotArham <arhamkabir231@gmail.com>
2026-09-20 18:48:35 -0400

    register controlller - 1

--- backend/auth/controllers.py
+++ backend/auth/controllers.py
@@ -1,6 +1,26 @@
 from database import get_connection
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
 
 ### Register
 
 def register(data):
-    return
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    hashed_password = password_hash.hash(data.password)
+
+    with open ("auth/sql/register.sql", "r") as file:
+        sql = file.read()
+
+    cursor.execute(
+        sql,
+        (data.email,data.username,hashed_password)
+    )
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+    return {"message:": "User registered successfully"}
